f6b8fb9c9a732fe43d87026e323c5f7f07861cd6
vasanth120999 <vasanthm100@yahoo.com>
2025-08-08 12:34:58 +0530

    Add files via upload

---
+++ OPERATORSss.py
@@ -0,0 +1,103 @@
+Python 3.12.9 (tags/v3.12.9:fdb8142, Feb  4 2025, 15:27:58) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=100
+b=200
+print("addition",a+b)
+addition 300
+print("subtraction",a-b)
+subtraction -100
+print("division",a/b)
+division 0.5
+print("floor division",a//b)
+floor division 0
+print("modules",a%b)
+modules 100
+print("exponentiation",a**b)
+exponentiation 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+print("multiplication",a*b)
+multiplication 20000
+
+a=500
+b=1000
+print(a>)
+SyntaxError: invalid syntax
+print(a>b)
+False
+print(a<b)
+True
+print(a<=b)
+True
+print(a>=b)
+False
+print(a==b)
+False
+print(a!=b)
+True
+a=car
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    a=car
+NameError: name 'car' is not defined. Did you mean: 'chr'?
+a='car'
+print("a in parking inthe place")
+a in parking inthe place
+print("a not in parking inthe place")
+a not in parking inthe place
+a=vasanth
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a=vasanth
+NameError: name 'vasanth' is not defined
+a="vasanth"
+b in
+SyntaxError: invalid syntax
+
+
+>>> a="vasanth"
+... b in a
+SyntaxError: multiple statements found while compiling a single statement
+>>> a="vasanth"
+... "b" in a
+SyntaxError: multiple statements found while compiling a single statement
+>>> 
+>>> 
+============================================================= RESTART: C:/Users/vasan/Downloads/UNIQ PHYTHON/operator 1.py =============================================================
+True
+>>> a="dhoni"
+>>> print("a" not in a)
+True
+>>> a=benz
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    a=benz
+NameError: name 'benz' is not defined
+>>> a=love
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    a=love
+NameError: name 'love' is not defined
+>>> a="BMW"
+>>> print("a" not in a)
+True
+>>> a=10
+>>> b=10
+>>> c=20
+>>> print(a is b)
+True
+>>> print(a is c)
+False
+>>> print(a not is c)
+SyntaxError: invalid syntax
+>>> print(a not c)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(a is c)
+False
+>>> 
+>>> a=10
+>>> b=10
+>>> c=20
+>>> print(a is not c)
+True
+a=10
+b=20
+print(a<10 or b>10)
